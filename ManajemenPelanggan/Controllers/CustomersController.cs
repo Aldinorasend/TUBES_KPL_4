37f4d5011cb21f4e9647263dbc775b8878af9736
Denanda <denandaputra00@gmail.com>
2024-05-17 21:13:30 +0700

    Manajemen API GetAll dan Post serta runtime

---
+++ ManajemenPelanggan/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using ManajemenPelanggan.Models;
+using ManajemenPelanggan.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ManajemenPelanggan.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CustomerRepository _repository;
+
+        public CustomersController()
+        {
+            _repository = new CustomerRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Customer customer)
+        {
+            _repository.Add(customer);
+            return CreatedAtAction(nameof(Get), new { id = customer.Id }, customer);
+        }
+    }
+}
---
+++ ManajemenPelanggan/Repositories/CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿using ManajemenPelanggan.Models;
+using Newtonsoft.Json;
+using System.Diagnostics.Contracts;
+
+
+namespace ManajemenPelanggan.Repositories
+{
+    public class CustomerRepository
+    {
+        private readonly string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "customers.json");
+
+        public List<Customer> GetAll()
+        {
+            Contract.Requires(File.Exists(filePath));
+            Contract.Ensures(Contract.Result<List<Customer>>() != null);
+
+            var jsonData = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<Customer>>(jsonData);
+        }
+
+
+        public void Add(Customer customer)
+        {
+            Contract.Requires(customer != null);
+            Contract.Ensures(GetAll().Contains(customer));
+
+            var customers = GetAll();
+            customers.Add(customer);
+            File.WriteAllText(filePath, JsonConvert.SerializeObject(customers));
+        }
+    }
+}
